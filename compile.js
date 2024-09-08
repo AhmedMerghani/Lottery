d97237698ec4ddca142647b4402e5d0696194f4f
AhmedMerghani <ahmed-merghani@outlook.com>
2024-09-08 17:46:44 +0400

    Initial Commit

---
+++ compile.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf-8');
+
+const input = {
+    language: 'Solidity',
+    sources: {
+        'Inbox.sol': {
+            content: source,
+        },
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*'], 
+            },
+        },
+    },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+const inboxContract = output.contracts['Inbox.sol']['Inbox'];
+const contractData = {
+    abi: inboxContract.abi,
+    bytecode: inboxContract.evm.bytecode.object
+};
+
+module.exports = contractData;
---
+++ deploy.js
@@ -0,0 +1,33 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const {abi, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider('canoe guard ensure sentence duck more dose west pair anger cause pet',
+    'https://eth-sepolia.g.alchemy.com/v2/tUCUOirXmytmdp664FyHOcCS_ZSQekYB');
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to use account ', accounts[0]);
+
+    const fixedGasLimit = 2000000;
+    const maxPriorityFeePerGas = await web3.eth.getGasPrice();
+    const maxFeePerGas = parseInt(maxPriorityFeePerGas, 10) + 1000000000;
+
+    try{
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: bytecode, arguments: ['hi there!'] })
+        .send({
+            gas: fixedGasLimit,
+            maxPriorityFeePerGas: maxPriorityFeePerGas.toString(),
+            maxFeePerGas: maxFeePerGas.toString(),
+            from: accounts[0]
+        });
+        console.log('Contract deployed to', result.options.address);
+    }
+    catch (error) {
+        console.log('Error deploying contract:', error.message);
+    }
+};
+deploy();
